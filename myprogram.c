5d91d2dd5070397db0544c6d7e85659940105f8a
Mahendra Deore <mahendradeore@gmail.com>
2016-03-15 02:28:06 +0530

    Fourth line added

--- myprogram.c
+++ myprogram.c
@@ -4,6 +4,7 @@ int main()
 printf("\nThis is my first git programme\n");
 printf("\nThis is Second line in prograame\n");
 printf("\nThis is third line in prograame\n");
+printf("\nThis is fourth line in prograame\n");
 return 0;
 
 }
