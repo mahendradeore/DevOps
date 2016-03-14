98a461ff54edceaa093c25bafdae7e1a02b3691b
Mahendra Deore <mahendradeore@gmail.com>
2016-03-15 01:51:47 +0530

    Adding two new files

--- program.c
+++ program.c
@@ -3,6 +3,7 @@ int main()
 {
 printf("\nThis is my first git programme\n");
 printf("\nThis is Second line in prograame\n");
+printf("\nThis is Third line in prograame\n");
 return 0;
 
 }
---
+++ test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int fun()
+{
+
+printf("\T This is another file \n");
+}
