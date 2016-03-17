417bb4f32eb868affbd95a0863810ca2b2a40602
Mahendra Deore <mahendradeore@gmail.com>
2016-03-17 09:35:26 +0530

    Merged Stash changes

--- myprogram.c
+++ myprogram.c
@@ -6,6 +6,7 @@ printf("\nThis is Second line in prograame\n");
 printf("\nThis is third line in prograame\n");
 printf("\nThis is fourth line in prograame\n");
 printf("\nThis is fifth line in prograame\n");
+printf("\nThis is sixth line in prograame\n");
 return 0;
 
 }
